b3e6793eb63575d4010b461e4fd40ca303e3afee
Josh Wang <spjay1@gmail.com>
2023-06-22 22:20:06 +0800

    chore: implement menu component

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,31 @@
 import React from "react";
 
+import {
+  TonicProvider,
+  PortalManager,
+  ToastManager,
+  Flex,
+} from "@tonic-ui/react";
+
+import { Menu } from "./Components";
+
 const App = () => {
-  return <div>Hello !!!!</div>;
+  return (
+    <TonicProvider
+      colorMode={{
+        defaultValue: "dark",
+      }}
+      useCSSBaseline
+    >
+      <PortalManager>
+        <ToastManager>
+          <Flex align="center" justify="center" minHeight="100vh">
+            <Menu />
+          </Flex>
+        </ToastManager>
+      </PortalManager>
+    </TonicProvider>
+  );
 };
 
 export default App;
---
+++ src/Components/Menu/Menu.jsx
@@ -0,0 +1,95 @@
+// Base sample:
+// https://trendmicro-frontend.github.io/tonic-ui/react/latest/components/menu#toggling-menus
+import React from "react";
+
+import ReactFocusLock from "react-focus-lock";
+
+import {
+  Menu as BaseMenu,
+  MenuList,
+  MenuItem,
+  MenuToggle,
+  Flex,
+  Box,
+  Icon,
+  Text,
+  useColorStyle,
+  useColorMode,
+} from "@tonic-ui/react";
+
+const Avatar = (props) => (
+  <Flex
+    p="1x"
+    borderRadius="50%"
+    alignItems="center"
+    justifyContent="center"
+    fontSize="xs"
+    lineHeight="1"
+    {...props}
+  />
+);
+
+const Menu = (props) => {
+  const [colorMode] = useColorMode();
+  const [colorStyle] = useColorStyle({ colorMode });
+
+  const wrapStyle = {
+    width: 200,
+    padding: 4,
+    background: colorStyle.background.primary,
+  };
+
+  return (
+    <Box {...wrapStyle}>
+      <BaseMenu>
+        <MenuToggle>
+          <Avatar
+            backgroundColor={colorStyle.background.secondary}
+            color={colorStyle.color.secondary}
+            _hover={{
+              color: colorStyle.color.primary,
+            }}
+          >
+            <Icon icon="user-team" size="5x" />
+          </Avatar>
+        </MenuToggle>
+        <ReactFocusLock persistentFocus>
+          <MenuList width="max-content">
+            {props.btnList.map((item) => {
+              return (
+                <MenuItem key={item.text}>
+                  <Flex flex="none" mr="3x">
+                    <Icon icon={item.icon} />
+                  </Flex>
+
+                  <Flex flex="auto">
+                    <Text>{item.text}</Text>
+                  </Flex>
+                </MenuItem>
+              );
+            })}
+          </MenuList>
+        </ReactFocusLock>
+      </BaseMenu>
+    </Box>
+  );
+};
+
+Menu.defaultProps = {
+  btnList: [
+    {
+      icon: "settings",
+      text: "Settings",
+    },
+    {
+      icon: "user-team",
+      text: "Accounts",
+    },
+    {
+      icon: "lock",
+      text: "Privacy control",
+    },
+  ],
+};
+
+export default Menu;
