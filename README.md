a4aa306b8a21ecdd4e897665d6e7f5205e24dfb1
Josh Wang <spjay1@gmail.com>
2023-06-22 13:03:01 +0800

    init: webpack, babel, react
    ref. https://jsramblings.com/creating-a-react-app-with-webpack/

---
+++ README.md
@@ -0,0 +1,14 @@
+# TXOne FED Assignment
+
+## Run server in development mode
+
+- `npm run dev`
+
+## Run server in production mode
+
+- `npm run build`
+- `npm run start`
+
+## References
+
+- [Creating a React app with Webpack](https://jsramblings.com/creating-a-react-app-with-webpack/)
---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    clean: true,
+    filename: "[name].[contenthash].bundle.js",
+    path: path.resolve(__dirname, "build"),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ["babel-loader"],
+      },
+    ],
+  },
+  resolve: {
+    extensions: ["*", ".js", ".jsx"],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.join(__dirname, "public", "index.html"),
+    }),
+  ],
+  devServer: {
+    static: {
+      directory: path.join(__dirname, "build"),
+    },
+    port: 3000,
+  },
+};
