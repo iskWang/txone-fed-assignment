9e6c1b0d69a44261c4b01a6999d07e0c073abf69
Josh Wang <spjay1@gmail.com>
2023-06-23 12:44:15 +0800

    remove Menu duplicate import

--- src/Components/Menu/Menu.test.js
+++ src/Components/Menu/Menu.test.js
@@ -1,4 +1,4 @@
-import { render, screen, fireEvent } from "@testing-library/react";
+import { render, fireEvent } from "@testing-library/react";
 import { TonicProvider } from "@tonic-ui/react";
 
 import Menu from "./Menu";
